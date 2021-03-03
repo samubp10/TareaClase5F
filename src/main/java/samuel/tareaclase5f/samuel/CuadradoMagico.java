8ce003dab435461e5f73fd4ec4346dff1b2e0bc1
samuel <samubp10@gmail.com>
2021-03-03 13:22:28 +0100

    Crea el constructor del CuadradoMagico

--- src/main/java/samuel/tareaclase5f/samuel/CuadradoMagico.java
+++ src/main/java/samuel/tareaclase5f/samuel/CuadradoMagico.java
@@ -5,10 +5,26 @@
  */
 package samuel.tareaclase5f.samuel;
 
+import java.util.Random;
+
 /**
  *
  * @author Samuel
  */
 public class CuadradoMagico {
-    
+
+    private int[][] cuadradoMagico;
+
+    public CuadradoMagico(int[][] cuadradoMagico) {
+        this.cuadradoMagico = new int[3][3];
+        Random random = new Random();
+        for (int i = 0; i < cuadradoMagico.length;) {
+            for (int j = 0; j < cuadradoMagico[i].length; j++) {
+
+                int enteroAleatorio = random.nextInt(100) + 1;
+            }
+        }
+
+    }
+
 }
